6eb1a6a23fc44133a8bc33f42be82948bc26b398
Sai Kalyan Veera <kalyanveera66@gmail.com>
2026-01-06 19:17:22 -0600

    streamlit code

---
+++ src/get_yt_video.py
@@ -0,0 +1,17 @@
+from youtubesearchpython import VideosSearch
+
+def get_yt_video(query):
+    videos_search = VideosSearch(query=query, limit=3)
+    result = videos_search.result()
+
+    video_titles = [video['title'] for video in result['result']]
+    video_links = [video['link'] for video in result['result']]
+
+    return video_titles, video_links
+
+
+# # ✅ TEST (optional – run only if file is executed directly)
+# if __name__ == "__main__":
+#     user_query = "Explain Adaptive Radiation"
+#     titles, links = get_yt_video(user_query)
+#     print(titles, links)
